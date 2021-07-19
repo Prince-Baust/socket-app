55b91fbb2f912b26e322b1e778425572caf1837c
Md. Maruf Al Hossain Prince <pmaruf42@gmail.com>
2021-07-19 21:31:24 +0600

    Socket initialized

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const expressServer = http.createServer(app);
+const {Server} = require('socket.io');
+const io = new Server(expressServer);
+
+io.on('connection', (socket) => {
+    console.log('New user connected.');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected.');
+    })
+})
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
