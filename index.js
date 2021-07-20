fa8c93dc39f7590fa5fd11d3364578058445a6dd
Md. Maruf Al Hossain Prince <pmaruf42@gmail.com>
2021-07-20 16:51:35 +0600

    client->server message send

--- index.js
+++ index.js
@@ -6,24 +6,28 @@ const {Server} = require('socket.io');
 const io = new Server(expressServer);
 
 io.on('connection', (socket) => {
-    console.log('New user connected.');
+    console.log('New user connected.');  // Log when user connects to localhost:3000
 
-    setInterval(() => {
-        let d = new Date();
-        let t = d.getTime();
-        // socket.send(t);
-        socket.emit('myEvent', t);
-    }, 1000);
+    socket.on('message', (msg) => {
+        console.log(msg);
+    });
+
+    // setInterval(() => {
+    //     let d = new Date();
+    //     let t = d.getTime();
+        // socket.send(t);              // Sending data using built-in event
+        // socket.emit('myEvent', t);  // Creating custom event
+    // }, 1000);
 
     socket.on('disconnect', () => {
-        console.log('User disconnected.');
-    })
-})
+        console.log('User disconnected.');    // Log when a user disconnected from localhost:3000
+    });
+});
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
-})
+});
 
 expressServer.listen(3000, ()=> {
     console.log('Server started @ 3000');
-})
+});
